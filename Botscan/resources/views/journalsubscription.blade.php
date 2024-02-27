85025e89f41b5581856f2eeea8e40a13d3d8ff5d
gopal dahatonde <dahatondegopal227@gmail.com>
2024-02-27 16:50:46 +0530

    pages modify journal subcription

--- Botscan/resources/views/journalsubscription.blade.php
+++ Botscan/resources/views/journalsubscription.blade.php
@@ -16,15 +16,15 @@
 
 
     <header class="header2">
-        <h1>BotScanis Journal Subscriptions</h1>
+        <h3><b>Journal Subscriptions</b></h3>
     </header>
 
     <section class="section2">
-        <h2>About Us</h2>
+        <h4>About Us</h4>
         <p>BotScanis provides the best journal subscription services in various subjects and specializations in animal sciences. Our experienced and professional team efficiently handles subscription work. Subscribers are provided with an online tracking and notification system to track the dispatched publications.</p>
         <p>Subscriptions are delivered through courier and registered post.</p>
 
-        <h2>Subscribe Now</h2>
+        <h4>Subscribe Now</h4>
         <p>If you're interested in staying updated with the latest research in animal sciences, subscribe to our journals now!</p>
         <a href="#" class="subscribe-btn2">Subscribe</a>
     </section>
