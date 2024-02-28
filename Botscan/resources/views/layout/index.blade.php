44df99e67e3c8983ab4d141bfdfcefd89cf70027
gopal dahatonde <dahatondegopal227@gmail.com>
2024-02-29 00:28:59 +0530

    footer correction    apply  hover effect in social media icon, add the client data

--- Botscan/resources/views/layout/index.blade.php
+++ Botscan/resources/views/layout/index.blade.php
@@ -359,59 +359,64 @@
         <div class=" col-sm-4 col-md col-sm-4  col-12 col">
           <h5 class="headin5_amrc col_white_amrc pt2">Find us</h5>
           <!--headin5_amrc-->
-          <p class="mb10">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has
-            been the industry's standard dummy text ever since the 1500s</p>
+          <p class="mb10"><i>Botscan Publication offers the most affordable self-publishing services in India. 
+            The Butscan publication is India's professional young writers with worldwide communities</i>
+         </p>
+
           <p><i class="fa fa-location-arrow"></i> 9878/25 sec 9 rohini 35 </p>
-          <p><i class="fa fa-phone"></i> +91-9999878398 </p>
-          <p><i class="fa fa fa-envelope"></i> info@example.com </p>
+          <p><i class="fa fa-phone"></i> +91 9021563292</p>
+          <p><i class="fa fa fa-envelope"></i> kuldevgroup@gmail.com </p>
 
 
         </div>
 
 
         <div class=" col-sm-4 col-md  col-6 col">
-          <h5 class="headin5_amrc col_white_amrc pt2">Quick links</h5>
+          <h5 class="headin5_amrc col_white_amrc pt2">Usefull Links</h5>
           <!--headin5_amrc-->
           <ul class="footer_ul_amrc">
-            <li><a href="#">Image Rectoucing</a></li>
-            <li><a href="#">Clipping Path</a></li>
-            <li><a href="#">Hollow Man Montage</a></li>
-            <li><a href="#">Ebay & Amazon</a></li>
-            <li><a href="#">Hair Masking/Clipping</a></li>
-            <li><a href="#">Image Cropping</a></li>
+            <li><a href="#">Home</a></li>
+            <li><a href="#">About us</a></li>
+            <li><a href="#"> Services </a></li>
+            <li><a href="#">Terms of service</a></li>
+            <li><a href="#">Privacy policy</a></li>
+            <li><a href="#">Guidlines</a></li>
           </ul>
           <!--footer_ul_amrc ends here-->
         </div>
 
 
         <div class=" col-sm-4 col-md  col-6 col">
-          <h5 class="headin5_amrc col_white_amrc pt2">Quick links</h5>
+          <h5 class="headin5_amrc col_white_amrc pt2">Our Services</h5>
           <!--headin5_amrc-->
           <ul class="footer_ul_amrc">
-            <li><a href="#">Remove Background</a></li>
-            <li><a href="#">Shadows & Mirror Reflection</a></li>
-            <li><a href="#">Logo Design</a></li>
-            <li><a href="#">Vectorization</a></li>
-            <li><a href="#">Hair Masking/Clipping</a></li>
-            <li><a href="#">Image Cropping</a></li>
+            <li><a href="#">Web Design</a></li>
+            <li><a href="#">Web Development</a></li>
+            <li><a href="#">Product Management</a></li>
+            <li><a href="#">Markting</a></li>
+            <li><a href="#">Graphic Design</a></li>
           </ul>
           <!--footer_ul_amrc ends here-->
         </div>
 
 
         <div class=" col-sm-4 col-md  col-12 col">
-          <h5 class="headin5_amrc col_white_amrc pt2">Follow us</h5>
+          <h5 class="headin5_amrc col_white_amrc pt2"><b>Contact us</b></h5>
           <!--headin5_amrc ends here-->
 
           <ul class="footer_ul2_amrc">
+
+          <li><a href="#"><i class="fas fa-home fleft padding-right"></i> </a>
+              <p>PL NO-1 GNO-16 HARISINGH NAGAR C.OP.H.S.SUDHAKAR 
+              <br> NG AURANGABAD- 431001, MAHARASHTRA, INDIA.</p>
+              </p>
+            </li>
+
             <li><a href="#"><i class="fab fa-twitter fleft padding-right"></i> </a>
-              <p>Lorem Ipsum is simply dummy text of the printing...<a href="#">https://www.lipsum.com/</a></p>
+              <p>For more details and information visit this Website : <a href="#">https://www.lipsum.com/</a></p>
             </li>
             <li><a href="#"><i class="fab fa-twitter fleft padding-right"></i> </a>
-              <p>Lorem Ipsum is simply dummy text of the printing...<a href="#">https://www.lipsum.com/</a></p>
-            </li>
-            <li><a href="#"><i class="fab fa-twitter fleft padding-right"></i> </a>
-              <p>Lorem Ipsum is simply dummy text of the printing...<a href="#">https://www.lipsum.com/</a></p>
+              <p><b>Contact</b> : <a> +91-9021563292,  +91-9325750894 </a></p>
             </li>
           </ul>
           <!--footer_ul2_amrc ends here-->
@@ -420,7 +425,7 @@
     </div>
 
 
-    <div class="container">
+     <div class="container">
       <ul class="foote_bottom_ul_amrc">
         <li><a href="#">Home</a></li>
         <li><a href="#">About</a></li>
@@ -428,9 +433,9 @@
         <li><a href="#">Pricing</a></li>
         <li><a href="#">Blog</a></li>
         <li><a href="#">Contact</a></li>
-      </ul>
+      </ul> 
       <!--foote_bottom_ul_amrc ends here-->
-      <p class="text-center">Copyright @2017 | Designed With by <a href="#">Your Company Name</a></p>
+      <p class="text-center">@ Copyright <b>BotScan</b> All rights Reserved <br> Designed With by <a href="#">@ngle Infotec</a></p>
 
       <ul class="social_footer_ul">
         <li><a href="#"><i class="fab fa-facebook-f"></i></a></li>
